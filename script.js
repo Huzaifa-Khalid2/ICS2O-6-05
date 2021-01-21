324b103f2c94276608c7523c501b1fa1b1b8d348
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-21 15:48:03 +0000

    too much

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 document.getElementById('my-btn').addEventListener('click', myFunction)
 function myFunction () {
-  let age = document.getElementById('my-text').value
+  let age = ''
+  let day = ''
+  age = document.getElementById('my-text').value
   age = parseInt(age)
-  let day = document.getElementById('my-text2').value
-  day = parseInt(day)
- if ((day === 'saturday' || day === 'sunday') && (age > 14)) {
+  day = document.getElementById('my-text2').value
+  if ((day === 'saturday') && (age > 7)) {
     alert('Time to enjoy the weekend!')
+  }  
+  else if ((day === 'friday') && (age > 5)) {
+    alert('Time to go to school!.')
+  } 
+  else if ((day === 'wednesday') && (age > 18)) {
+    alert('Time to go to work!.')
+  } 
+  else if ((day === 'sunday') && (age > 18)) {
+    alert('Time to enjoy the weekend!.') 
+  } 
+  else {
+    alert('ummmmmm you are too young to got work or school so congratulations! :)')
   }
-}
+  alert('Thank you for verifying your age!')
+}  
