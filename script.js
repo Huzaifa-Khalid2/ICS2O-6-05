95e11d7a90a1019552a802fb2667350e9c0636eb
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-23 20:54:22 +0000

    too much

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function myFunction () {
   day = document.getElementById('my-text2').value
   if ((day === 'saturday') && (age > 7)) {
     alert('Time to enjoy the weekend!')
-  }  
+ }
   else if ((day === 'friday') && (age > 5)) {
     alert('Time to go to school!.')
-  } 
+  }
   else if ((day === 'wednesday') && (age > 18)) {
     alert('Time to go to work!.')
-  } 
+  }
   else if ((day === 'sunday') && (age > 18)) {
-    alert('Time to enjoy the weekend!.') 
-  } 
+    alert('Time to enjoy the weekend!.')
+  }
   else {
     alert('ummmmmm you are too young to got work or school so congratulations! :)')
   }
   alert('Thank you for verifying your age!')
-}  
+}
